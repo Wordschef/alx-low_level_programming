fb98132b63d2a46a40147fd4e7e70e37d3d3d0e8
wordschef <kinggeorgagbo@gmail.com>
2023-06-22 12:56:36 -0700

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,15 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b && b > c)
 {
 largest = a;
 }
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
+else if (b > a && a > c)
 {
 largest = b;
 }
-lse
+else
 {
 largest = c;
 }
