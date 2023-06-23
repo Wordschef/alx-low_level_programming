c0ad62f2ce1902663f2567fc5cf151c8532b2bfd
wordschef <kinggeorgagbo@gmail.com>
2023-06-23 05:19:30 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+Task 6. 6-print_line.c
+
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times.
+ *
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
+
